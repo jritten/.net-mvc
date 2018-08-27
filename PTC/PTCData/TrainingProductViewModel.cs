e06c533c12fe6b319e81e4a8bbcd605991398d12
Vilde Vevatne <v-viveva@microsoft.com>
2018-08-27 13:27:43 -0700

    Adds view model class

---
+++ PTC/PTCData/TrainingProductViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTCData
+{
+    public class TrainingProductViewModel
+    {
+        public TrainingProductViewModel()
+        {
+            Products = new List<TrainingProduct>();
+        }
+        public List<TrainingProduct> Products { get; set; }
+        public void Get()
+        {
+            TrainingProductManager mgr = new TrainingProductManager();
+            Products = mgr.Get();
+        }
+    }
+}
