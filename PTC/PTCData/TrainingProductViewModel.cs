115bca7f9346877b1352c9a2292af1af476ddc22
Vilde Vevatne <v-viveva@microsoft.com>
2018-08-27 14:00:40 -0700

    Adds search entity attribute

--- PTC/PTCData/TrainingProductViewModel.cs
+++ PTC/PTCData/TrainingProductViewModel.cs
@@ -11,21 +11,32 @@ namespace PTCData
         public TrainingProductViewModel()
         {
             Products = new List<TrainingProduct>();
+            SearchEntity = new TrainingProduct();
             EventCommand = "List";
         }
         public string EventCommand { get; set; }
         public List<TrainingProduct> Products { get; set; }
+        public TrainingProduct SearchEntity { get; set; }
         public void HandleRequest()
         {
             switch(EventCommand.ToLower())
             {
                 case "list":
+                case "search":
                     Get();
+                    break;
+
+                case "resetsearch":
+                    ResetSearch();
                     break;
 
                 default:
                     break;
             }
+        }
+        private void ResetSearch()
+        {
+            SearchEntity = new TrainingProduct();
         }
         private void Get()
         {
